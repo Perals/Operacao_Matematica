61d4e2ab88d54fbde45bffd7b1579ea7b7d91117
Rafael Peral <Rafael Peral@RAFAEL>
2025-08-30 20:04:56 -0300

    Adicionar arquivos de projeto.

---
+++ Operacao_Matematica/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security;
+
+namespace Operacao_Matematica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string operacao;
+            int n1,n2;
+            double resultado;
+
+            Console.WriteLine("Digite o seu primeiro nome:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Escolha a operação a ser feita (+.-./.*)");
+            operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro número");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            n2 = int.Parse(Console.ReadLine());
+
+
+            switch (operacao)
+            {
+                case "+":
+                    resultado = n1 + n2;
+                    Console.WriteLine($"A soma é:  {resultado} ");
+                    break;
+                default:
+                    Console.WriteLine("Operação não encontrada");
+                    break;
+            }
+        }
+    }
+}
