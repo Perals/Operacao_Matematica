11f01009668520c4b83d1bf687be9071698f7d93
perals <rafaelperal.ads@gmail.com>
2025-09-04 15:08:08 -0300

    Foi feita a inclusão da operação de Raiz e resto da divisão.

--- Operacao_Matematica/Program.cs
+++ Operacao_Matematica/Program.cs
@@ -8,55 +8,75 @@ namespace Operacao_Matematica
         {
             string name;
             string operacao;
-            int n1,n2;
-            double resultado;
+            double n1,n2, resultado, resto;
 
             Console.WriteLine("Digite o seu primeiro nome:");
             name = Console.ReadLine();
 
-            Console.WriteLine("Escolha a operação: (+ Soma, - Subtração, * Multiplicação, / Divisão, ^ Potência)");
+            Console.WriteLine("Escolha a operação: (+ Soma, - Subtração, * Multiplicação, / Divisão, ^ Potência, v Raiz Quadrada)");
             operacao = Console.ReadLine();
 
-            Console.WriteLine("Digite o primeiro número");
-            n1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número");
-            n2 = int.Parse(Console.ReadLine());
 
 
-            switch (operacao)
+            if (operacao == "v")
             {
-                case "+":
-                    resultado = n1 + n2;
-                    Console.WriteLine($"A soma é :  {resultado} ");
-                    break;
+                Console.WriteLine("Digite o número que deseja saber a Raiz Quadrada: ");
+                n1 = double.Parse(Console.ReadLine());
 
-                case "-":
-                    resultado = n1 - n2;
-                    Console.WriteLine($"A subtração é :  {resultado} ");
-                    break;
-                case "/":
-                    if (n1 == 0 || n2 == 0)
-                    {
-                        Console.WriteLine("Não é possivel dividir por zero");
-                    }
-                    else
-                    {
-                        resultado = n1 / n2;
-                        Console.WriteLine($"A Divisão é :  {resultado}");
-                    }
-                     break;                    
-                case "*":
-                    resultado = n1 * n2;
-                    Console.WriteLine($" A Multiplicação é : {resultado}");
-                    break;
-                case "^":
-                    resultado = Math.Pow( n1, n2 );
-                    Console.WriteLine($"A Potenciação é : {resultado}");
-                    break;
-                default:
-                    Console.WriteLine("Operação não encontrada");
-                    break;
+                if (n1 < 0)
+                {
+                    Console.WriteLine("Não é possivel calcular a raiz de numeros menores do que zero");
+                }
+                else
+                {
+                    resultado = Math.Sqrt(n1);
+                    Console.WriteLine($"A rais quadrada do número é : {resultado}");
+                }
+            }
+            else {
+
+                Console.WriteLine("Digite o primeiro número");
+                n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo número");
+                n2 = double.Parse(Console.ReadLine());
+
+                switch (operacao)
+                {
+                    case "+":
+                        resultado = n1 + n2;
+                        Console.WriteLine($"A soma é :  {resultado} ");
+                        break;
+
+                    case "-":
+                        resultado = n1 - n2;
+                        Console.WriteLine($"A subtração é :  {resultado} ");
+                        break;
+                    case "/":
+                        if (n1 == 0 || n2 == 0)
+                        {
+                            Console.WriteLine("Não é possivel dividir por zero");
+                        }
+                        else
+                        {
+                            resultado = n1 / n2;
+                            resto = n1 % n2;
+                            Console.WriteLine($"A Divisão é :  {resultado}");
+                            Console.WriteLine($"O resto da divisão é: {resto}");
+                        }
+                        break;
+                    case "*":
+                        resultado = n1 * n2;
+                        Console.WriteLine($" A Multiplicação é : {resultado}");
+                        break;
+                    case "^":
+                        resultado = Math.Pow(n1, n2);
+                        Console.WriteLine($"A Potenciação é : {resultado}");
+                        break;
+                    default:
+                        Console.WriteLine("Operação não encontrada");
+                        break;
+                }
             }
         }
     }
